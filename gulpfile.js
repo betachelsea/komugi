226bdd7c1f6dfef94a8450f51815bc0b9f14d436
beta_chelsea <beta.chelsea@gmail.com>
2015-04-11 14:56:07 +0900

    Add gulp-concat setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp"),
+    concat = require('gulp-concat'),
     concatCSS = require('gulp-concat-css'),
+    tap = require('gulp-tap'),
     sass = require("gulp-sass");
 
 gulp.task("sass", function() {
@@ -9,3 +11,21 @@ gulp.task("sass", function() {
       .pipe(gulp.dest('./public/'));
 });
 
+gulp.task("js", function() {
+  // 読み込み順
+  var js_resources = [
+    'source/vendor/js/**/*.js',
+    'source/js/**/*.js'
+  ];
+  gulp.src(js_resources)
+      .pipe(tap(function(file) {
+        file.contents = Buffer.concat([
+          new Buffer("(function(){\n"),
+          new Buffer("'use strict';\n"),
+          file.contents,
+            new Buffer("})();")
+        ]);
+      }))
+      .pipe(concat('application.js'))
+      .pipe(gulp.dest('./public/'));
+});
