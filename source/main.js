e855c100293bb3ed17e496e23993cfd09d353a99
YutaGoto <you.goto.510@gmail.com>
2015-04-12 09:50:26 +0900

    add jquery

--- source/main.js
+++ source/main.js
@@ -85,6 +85,18 @@ var main = function() {
   });
 
   //map.render();
+
+  $(function(){
+  	$('a[href^=#]').click(function(){
+  		var speed = 500;
+  		var href= $(this).attr("href");
+  		var target = $(href == "#" || href == "" ? 'html' : href);
+  		var position = target.offset().top;
+  		$("html, body").animate({scrollTop:position}, speed, "swing");
+  		return false;
+  	});
+  });
+  
 };
 
 $(function() {
