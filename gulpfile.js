40868b44a2b300388bd94f1b3c78080368ab2255
beta_chelsea <beta.chelsea@gmail.com>
2015-04-29 15:41:43 +0900

    server task 追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require("gulp"),
+    server = require('gulp-express'),
     concat = require('gulp-concat'),
     concatCSS = require('gulp-concat-css'),
     tap = require('gulp-tap'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
     sass = require("gulp-sass");
+
+gulp.task('server', function() {
+  server.run(['./bin/www']);
+});
 
 gulp.task("sass", function() {
   gulp.src("source/sass/**/*scss")
@@ -21,4 +26,4 @@ gulp.task("js", function() {
   .pipe(gulp.dest('./public/'));
 });
 
-gulp.task("default", ['sass', 'js']);
+gulp.task("default", ['sass', 'js', 'server']);
